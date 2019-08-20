e2bedd4f4a4612294af784d737737fceacbb7046
Edward Iga Kigongo <igakigongo@gmail.com>
2019-08-20 11:49:14 +0200

    Counting Bits - Memoized solution

--- counting_bits.rb
+++ counting_bits.rb
@@ -3,8 +3,26 @@
 
 # @param {Integer} num
 # @return {Integer[]}
-def count_bits(num)
+def count_bits_basic(num)
   (0..num).map { |x| x.to_s(2).split("").count("1") }
 end
 
-p count_bits(10)
+@memo = {}
+
+def counter(num)
+  return @memo[num] if @memo.has_key?(num)
+  return num if [0, 1].include?(num)
+  result = counter(num / 2) + ((num % 2).positive? ? 1 : 0)
+  @memo[num] = result
+  return result
+end
+
+def count_bits(num)
+  (0..num).map(&method(:counter))
+end
+
+p count_bits_basic(10)
+# p count_bits(3)
+p count_bits(7)
+p count_bits(28)
+p count_bits(31)
