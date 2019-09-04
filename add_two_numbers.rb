6ba7d86ae07e20251b1aa681d7a1916afac846c0
Edward Iga Kigongo <igakigongo@gmail.com>
2019-09-04 03:27:52 +0200

    Improve on add_two_numbers code

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -1,3 +1,4 @@
+# Definition for singly-linked list.
 class ListNode
   attr_accessor :val, :next
 
@@ -7,44 +8,30 @@ class ListNode
   end
 end
 
-def to_number(list_node)
-  number, multiplier = 0, 1
-  node = list_node
-  while !node.nil?
-    number += node.val * multiplier
-    multiplier *= 10
-    node = node.next
-  end
-  number
-end
-
-def get_tail_node(node)
-  return nil if node.nil?
-  current = node
-  until current.next.nil?
-    current = current.next
-  end
-  current
-end
-
-def to_list(number)
-  n, node = number.to_s.reverse!, nil
-  start, len = 0, n.length
-  while start < len
-    tail = get_tail_node(node)
-    new_node = ListNode.new(n[start].to_i)
-    if tail.nil?
-      node = new_node
-    else
-      tail.next = new_node
-    end
-    start += 1
-  end
-  node
-end
-
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
 def add_two_numbers(l1, l2)
-  to_list(to_number(l1) + to_number(l2))
+  # state of resultant linked list
+  head, tail = nil, nil
+
+  carry, result = 0, ""
+  until l1.nil? && l2.nil? && carry.zero?
+    v1, v2 = (l1&.val || 0), (l2&.val || 0)
+    sum = v1 + v2 + carry # old carry value
+    carry = sum / 10
+
+    # insert operation with O(1) time
+    node = ListNode.new(sum % 10)
+    if head.nil?
+      head = tail = node
+    else
+      tail.next = node
+      tail = node
+    end
+    l1, l2 = l1&.next, l2&.next
+  end
+  head
 end
 
 # create some variables
@@ -56,4 +43,18 @@ l2 = ListNode.new(5)
 l2.next = ListNode.new(6)
 l2.next.next = ListNode.new(4)
 
-p to_list(to_number(l1) + to_number(l2))
+l3 = ListNode.new(5)
+l3.next = ListNode.new(0)
+l3.next.next = ListNode.new(4)
+l3.next.next.next = ListNode.new(2)
+
+l4 = ListNode.new(5)
+l4.next = ListNode.new(8)
+l4.next.next = ListNode.new(4)
+
+l5 = ListNode.new(9)
+l6 = ListNode.new(9)
+
+p add_two_numbers(l1, l2)
+p add_two_numbers(l3, l4)
+p add_two_numbers(l5, l6)
